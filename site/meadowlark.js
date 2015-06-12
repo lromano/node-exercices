7d48b137f02df4000daef9a6f2d36ca91f299f1b
Lucas Romano <lucas.romano@ngahr.com>
2015-06-12 10:05:54 +0200

    chapter6

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -20,6 +20,15 @@ app.use(function(req, res, next){
   next();
 });
 
+// app.get("/headers", function(req, res){
+//   res.set("Content-Type", "text/plain");
+//   var s = '';
+//   for(var name in req.headers) {
+//     s+= name + ": " + req.headers[name] + "\n";
+//   }
+//   res.send(s);
+// });
+
 app.get("/", function(req, res){
   res.render("home");
 });
